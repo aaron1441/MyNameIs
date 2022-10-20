b698be8b2d2b6a87ab6b68a65cfe73d81057b4a5
Aaron Dee <aarondee2014@gmail.com>
2022-10-20 14:15:32 +0100

    Comments added

--- src/ie/atu/sw/Output.java
+++ src/ie/atu/sw/Output.java
@@ -1,8 +1,14 @@
 package ie.atu.sw;
 
 public class Output {
+	// private variable state to choose which thread to run
 	private int state=1;
+	
+	// synchronized methods to display the words
+	// these methods are synchronized so that they display the words in the correct order
+	// it can be written like this but this is rather long and alot of methods
 	public synchronized void displayWord1(String msg) {
+		// if the state is not 1 then wait
 		while(state!=1) {
 			try {
 				wait();
@@ -11,8 +17,11 @@ public class Output {
 				e.printStackTrace();
 			}
 		}
+		//  if the state is 1 then print the message
 		System.out.println(msg);
+		// set the state to 2
 		state=2;
+		// wake any methods that are waiting
 		notifyAll();
 	}
 	public synchronized void displayWord2(String msg) {
@@ -54,6 +63,8 @@ public class Output {
 		state=4;
 		notifyAll();
 	}
+	
+	//  this method 
 	public synchronized void display(String msg, int Desiredstate)
 	{
 		while(state != Desiredstate)
@@ -65,6 +76,7 @@ public class Output {
 				e.printStackTrace();
 			}
 		}
+			//System.out.println("Here");
 			System.out.println(msg);
 			state = state +1;
 			
--- src/ie/atu/sw/test.java
+++ src/ie/atu/sw/test.java
@@ -4,11 +4,13 @@ public class test {
 	public static void main(String[] args) {
 		Output outputScreen = new Output();
 		
+		// declare each thread and pass it the word to use and the shared output screen
 		DisplayThread t1 = new DisplayThread(outputScreen,1,"My");
 		DisplayThread t2 = new DisplayThread(outputScreen,2,"Name");
 		DisplayThread t3 = new DisplayThread(outputScreen,3,"Is");
 		DisplayThread t4 = new DisplayThread(outputScreen,4,"Bob");
 		
+		// run each thread
 		t1.start();
 		t2.start();
 		t3.start();
